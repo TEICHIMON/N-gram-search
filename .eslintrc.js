cc76e1455d4d77a3271df59d98e2373caf574876
TEICHIMON <teichimon@gmail.com>
2024-06-11 20:24:37 +0900

    fix[project]: fix favicon 404 errors
    [fixed: favicon 404]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
+
   extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   env: {
     "vue/setup-compiler-macros": true,
     node: true,
   },
+  ignorePatterns: ["./frontend/dist", ".eslintrc.cjs"],
 };
